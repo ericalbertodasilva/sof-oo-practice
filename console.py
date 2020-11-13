259cd12b37a4dfc5120e7583cbad574d68026159
ericalbertodasilva <ercker@gmail.com>
2020-11-12 21:37:59 -0300

    review of the class with sole responsibility

--- console.py
+++ console.py
@@ -9,3 +9,23 @@ class CashMachineConsole:
     @staticmethod
     def call_operation():
         option_typed = CashMachineConsole.get_menu_options_typed()
+        CashMachineOperation.do_operation(option_typed)
+
+class CashMachineOperation:
+    
+    @staticmethod
+    def do_operation(option):
+        if option == '1':
+            ShowBalanceOperation.do_operation();
+        elif option == '2':
+            WithDrawOperation.do_operation();
+
+class ShowBalanceOperation:
+    @staticmethod
+    def do_operation():
+        print('Mostrar saldo')
+
+class  WithDrawOperation:
+    @staticmethod
+    def do_operation():
+        print('Sacar dinheiro')
